931cf64b2db01a3effecf43e37b30f7021c25047
Arif Ozan AKTAŞ <aoaktas@hotmail.com>
2024-10-18 21:20:35 +0300

    Add project files.

---
+++ ExtensionMethods/Extensions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExtensionMethods;
+internal static class Extensions
+{
+    public static string GetFirstCharacter(this string input)
+    {
+        return input.Substring(0, 1);
+    }
+    public static string GetSeoUrl(this string text)
+    {
+        text = text.Replace(" ","-").ToLower();
+        return text;
+    }
+}
---
+++ ExtensionMethods/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace ExtensionMethods;
+
+internal class Program
+{
+    
+    static void Main(string[] args)
+    {
+        string name = "asp.net core";
+        Console.WriteLine(name.GetFirstCharacter());
+
+ 
+    }
+}
